c5d94e9d30bf7293b92f59710bea9831fc1cfeff
Brandon Stone <brandonn.stone@gmail.com>
2017-04-20 14:23:07 -0600

    Add scripts
    Adding C# files for all characters, objects, whatever.

---
+++ Scripts/enemyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+	public Transform target;
+  public float speed;
+
+	void FixedUpdate() {
+    float step = speed * Time.deltaTime;
+    transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+  }
+}
